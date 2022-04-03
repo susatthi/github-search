d8a0414392c7fdb00266e533d06fe24a9fc67889
susa.keyber <susa@keyber.jp>
2022-04-03 16:45:09 +0900

    #44 impl Web版のURLの#を除去する

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,16 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_search/config/github_search_app.dart';
 
+import 'utils/url_strategy/url_strategy.dart';
+
 void main() {
   // CacheManagerのログレベルを設定する
   CacheManager.logLevel =
       kDebugMode ? CacheManagerLogLevel.verbose : CacheManagerLogLevel.none;
+
+  // Web版のURLの#を除去する
+  // see: https://docs.flutter.dev/development/ui/navigation/url-strategies
+  usePathUrlStrategy();
 
   runApp(
     ProviderScope(
