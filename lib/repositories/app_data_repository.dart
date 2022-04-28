f5f7450b0314ebeaf2767ea48934aaf21ab32f21
susa.keyber <susa@keyber.jp>
2022-04-28 11:47:05 +0900

    #94 fix enumのプロパティ名を変更

--- lib/repositories/app_data_repository.dart
+++ lib/repositories/app_data_repository.dart
@@ -16,8 +16,8 @@ final appDataRepositoryProvider = Provider<AppDataRepository>(
 abstract class AppDataRepository {
   /// デフォルト値
   static const defaultValues = <AppDataKey, Object?>{
-    AppDataKey.searchReposSort: RepoSearchReposSort.bestMatch,
-    AppDataKey.searchReposOrder: RepoSearchReposOrder.desc,
+    AppDataKey.repoSearchReposSort: RepoSearchReposSort.bestMatch,
+    AppDataKey.repoSearchReposOrder: RepoSearchReposOrder.desc,
   };
 
   /// リポジトリ検索用ソートを設定する
@@ -35,6 +35,9 @@ abstract class AppDataRepository {
 
 /// アプリデータのキー名
 enum AppDataKey {
-  searchReposSort,
-  searchReposOrder,
+  /// リポジトリ検索ソート値
+  repoSearchReposSort,
+
+  /// リポジトリ検索オーダー値
+  repoSearchReposOrder,
 }
