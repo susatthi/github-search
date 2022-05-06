262b3210ef51633bfadcad1960c44a8954d48f0a
susa.keyber <susa@keyber.jp>
2022-05-07 06:08:27 +0900

    feat: ロガーパッケージを logger から roggle に変更
    Issue #102

--- test/utils/logger_test.dart
+++ test/utils/logger_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github_search/utils/logger.dart';
-import 'package:logger/logger.dart';
 
 void main() {
   group('logger', () {
@@ -20,7 +19,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e('message');
       //ignore: cascade_invocations
-      logger.wtf('message');
+      expect(
+        () {
+          logger.wtf('message');
+        },
+        throwsAssertionError,
+      );
     });
     test('【String】ログが出力されるはず（エラーとStackTrace付き）', () async {
       //ignore: cascade_invocations
@@ -34,7 +38,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e('message', Exception('例外'), StackTrace.current);
       //ignore: cascade_invocations
-      logger.wtf('message', Exception('例外'), StackTrace.current);
+      expect(
+        () {
+          logger.wtf('message', Exception('例外'), StackTrace.current);
+        },
+        throwsAssertionError,
+      );
     });
     test('【int】ログが出力されるはず', () async {
       //ignore: cascade_invocations
@@ -48,7 +57,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e(100);
       //ignore: cascade_invocations
-      logger.wtf(100);
+      expect(
+        () {
+          logger.wtf(100);
+        },
+        throwsAssertionError,
+      );
     });
     test('【double】ログが出力されるはず', () async {
       //ignore: cascade_invocations
@@ -62,7 +76,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e(3.14);
       //ignore: cascade_invocations
-      logger.wtf(3.14);
+      expect(
+        () {
+          logger.wtf(3.14);
+        },
+        throwsAssertionError,
+      );
     });
     test('【bool】ログが出力されるはず', () async {
       //ignore: cascade_invocations
@@ -76,7 +95,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e(true);
       //ignore: cascade_invocations
-      logger.wtf(true);
+      expect(
+        () {
+          logger.wtf(true);
+        },
+        throwsAssertionError,
+      );
     });
     test('【List<String>】ログが出力されるはず', () async {
       //ignore: cascade_invocations
@@ -90,7 +114,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e(['apple', 'banana']);
       //ignore: cascade_invocations
-      logger.wtf(['apple', 'banana']);
+      expect(
+        () {
+          logger.wtf(['apple', 'banana']);
+        },
+        throwsAssertionError,
+      );
     });
     test('【Map<String, String>】ログが出力されるはず', () async {
       //ignore: cascade_invocations
@@ -104,7 +133,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e({'key': 'name', 'value': 'apple'});
       //ignore: cascade_invocations
-      logger.wtf({'key': 'name', 'value': 'apple'});
+      expect(
+        () {
+          logger.wtf({'key': 'name', 'value': 'apple'});
+        },
+        throwsAssertionError,
+      );
     });
     test('【dynamic Function()】ログが出力されるはず', () async {
       //ignore: cascade_invocations
@@ -118,124 +152,12 @@ void main() {
       //ignore: cascade_invocations
       logger.e(() => '関数です');
       //ignore: cascade_invocations
-      logger.wtf(() => '関数です');
+      expect(
+        () {
+          logger.wtf(() => '関数です');
+        },
+        throwsAssertionError,
+      );
     });
-  });
-  group('シンプルlogger', () {
-    _stringTest(
-      '【String】ログが出力されるはず',
-      Logger(
-        printer: SinglePrettyPrinter(
-          colors: false,
-          printCaller: false,
-          printEmojis: false,
-          printLevels: false,
-          printTime: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋ロガー名',
-      Logger(
-        printer: SinglePrettyPrinter(
-          loggerName: '[APP]',
-          colors: false,
-          printCaller: false,
-          printEmojis: false,
-          printLevels: false,
-          printTime: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋カラー',
-      Logger(
-        printer: SinglePrettyPrinter(
-          printCaller: false,
-          printEmojis: false,
-          printLevels: false,
-          printTime: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋呼び出し場所',
-      Logger(
-        printer: SinglePrettyPrinter(
-          colors: false,
-          printEmojis: false,
-          printLevels: false,
-          printTime: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋絵文字',
-      Logger(
-        printer: SinglePrettyPrinter(
-          colors: false,
-          printCaller: false,
-          printLevels: false,
-          printTime: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋ログレベル',
-      Logger(
-        printer: SinglePrettyPrinter(
-          colors: false,
-          printCaller: false,
-          printEmojis: false,
-          printTime: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋タイムスタンプ',
-      Logger(
-        printer: SinglePrettyPrinter(
-          colors: false,
-          printCaller: false,
-          printEmojis: false,
-          printLevels: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋ロガー名＋呼び出し場所',
-      Logger(
-        printer: SinglePrettyPrinter(
-          loggerName: '[APP]',
-          colors: false,
-          printEmojis: false,
-          printLevels: false,
-          printTime: false,
-        ),
-      ),
-    );
-    _stringTest(
-      '【String】ログが出力されるはず＋デフォルト',
-      Logger(
-        printer: SinglePrettyPrinter(),
-      ),
-    );
-  });
-}
-
-void _stringTest(Object description, Logger logger) {
-  test(description, () async {
-    //ignore: cascade_invocations
-    logger.v('message');
-    //ignore: cascade_invocations
-    logger.d('message');
-    //ignore: cascade_invocations
-    logger.i('message');
-    //ignore: cascade_invocations
-    logger.w('message');
-    //ignore: cascade_invocations
-    logger.e('message');
-    //ignore: cascade_invocations
-    logger.wtf('message');
   });
 }
