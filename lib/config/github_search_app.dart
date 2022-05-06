12a4df91764ca9a345f07818960f63f71c09f75a
susa.keyber <susa@keyber.jp>
2022-05-07 07:49:47 +0900

    🧹 refactor: ルーティングを別ファイルに切り出すと単体テストが通らなくなったので元に戻す
    Issue #108

--- lib/config/github_search_app.dart
+++ lib/config/github_search_app.dart
@@ -2,12 +2,18 @@
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../assets/fonts.gen.dart';
 import '../localizations/strings.g.dart';
-import 'router.dart';
+import '../presentation/components/repo/repo_detail_view_notifier.dart';
+import '../presentation/pages/error/error_page.dart';
+import '../presentation/pages/repo/repo_index_page.dart';
+import '../presentation/pages/repo/repo_view_page.dart';
 
 /// GitHubSearch アプリ
 class GitHubSearchApp extends StatelessWidget {
@@ -31,7 +37,7 @@ class GitHubSearchApp extends StatelessWidget {
 }
 
 class _GitHubSearchApp extends StatelessWidget {
-  const _GitHubSearchApp({
+  _GitHubSearchApp({
     Key? key,
     this.home,
   }) : super(key: key);
@@ -58,8 +64,8 @@ class _GitHubSearchApp extends StatelessWidget {
     }
 
     return MaterialApp.router(
-      routerDelegate: router.routerDelegate,
-      routeInformationParser: router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      routeInformationParser: _router.routeInformationParser,
       localizationsDelegates: GlobalMaterialLocalizations.delegates,
       supportedLocales: LocaleSettings.supportedLocales,
       locale: TranslationProvider.of(context).flutterLocale,
@@ -68,4 +74,39 @@ class _GitHubSearchApp extends StatelessWidget {
       theme: theme,
     );
   }
+
+  /// 画面遷移の定義
+  final _router = GoRouter(
+    initialLocation: RepoIndexPage.path,
+    routes: [
+      // リポジトリ一覧画面
+      GoRoute(
+        path: RepoIndexPage.path,
+        name: RepoIndexPage.name,
+        builder: (context, state) => const RepoIndexPage(),
+        routes: [
+          // リポジトリ詳細画面
+          GoRoute(
+            path: RepoViewPage.path,
+            name: RepoViewPage.name,
+            builder: (context, state) => ProviderScope(
+              overrides: [
+                repoDetailViewStateProvider.overrideWithProvider(
+                  repoDetailViewStateProviderFamily(
+                    RepoDetailViewParameter.from(state),
+                  ),
+                ),
+              ],
+              child: const RepoViewPage(),
+            ),
+          ),
+        ],
+      ),
+    ],
+    // エラー画面
+    errorBuilder: (context, state) => ErrorPage(
+      error: state.error,
+    ),
+    debugLogDiagnostics: !kReleaseMode,
+  );
 }
