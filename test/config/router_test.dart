486aef81fb42ec3f72e1dd71f5486eb0a31f5b9b
susa.keyber <susa@keyber.jp>
2022-05-11 13:19:27 +0900

    🚨 test: テストの追加
    Issue #112

--- test/config/router_test.dart
+++ test/config/router_test.dart
@@ -5,7 +5,9 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:github_search/presentation/pages/error/error_page.dart';
 import 'package:github_search/presentation/pages/repo/repo_index_page.dart';
+import 'package:go_router/go_router.dart';
 
 import '../test_utils/hive.dart';
 import '../test_utils/mocks.dart';
@@ -20,11 +22,22 @@ void main() {
     await closeAppDataBox(tmpDir);
   });
 
-  group('GitHubSearchApp', () {
-    testWidgets('起動出来るはず', (tester) async {
+  group('routerProvider', () {
+    testWidgets('存在しないパスに遷移するとエラー画面を表示するはず', (tester) async {
       await tester.pumpWidget(mockGitHubSearchApp());
       await tester.pumpAndSettle();
+
       expect(find.byType(RepoIndexPage), findsOneWidget);
+      expect(find.byType(ErrorPage), findsNothing);
+
+      // 存在しないパスに遷移する
+      final state = tester.firstState(find.byType(RepoIndexPage));
+      state.context.go('/unknown');
+      await tester.pump();
+
+      // エラー画面が表示されるはず
+      await tester.pump();
+      expect(find.byType(ErrorPage), findsOneWidget);
     });
   });
 }
---
+++ test/presentation/components/common/cached_circle_avatar_test.dart
@@ -0,0 +1,79 @@
+// Copyright 2022 susatthi All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:github_search/presentation/components/common/cached_circle_avatar.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../test_utils/mocks.dart';
+
+class MockDefaultCacheManager extends Mock implements DefaultCacheManager {}
+
+void main() {
+  const dummyUrl = 'https://keyber.jp/images/logo_white.png';
+  late DefaultCacheManager mockCacheManager;
+  setUp(() {
+    mockCacheManager = MockDefaultCacheManager();
+  });
+
+  group('CachedCircleAvatar', () {
+    testWidgets('ダミー画像が表示されるはず', (tester) async {
+      // UnitTestでimageBuilderでダミー画像を表示する方法が存在しないようなので
+      // テストは未実施です。下記のIssueで報告されているがいまだにOpenのままです。
+      //
+      // https://github.com/Baseflow/flutter_cache_manager/issues/290
+    });
+    testWidgets('エラーが表示されるはず', (tester) async {
+      await tester.pumpWidget(
+        mockGitHubSearchApp(
+          home: CachedCircleAvatar(
+            cacheManager: mockCacheManager,
+            url: dummyUrl,
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      expect(find.byIcon(Icons.error), findsOneWidget);
+    });
+    testWidgets('loading=trueでローディングが表示されるはず', (tester) async {
+      await tester.pumpWidget(
+        mockGitHubSearchApp(
+          home: CachedCircleAvatar(
+            cacheManager: mockCacheManager,
+            url: dummyUrl,
+          ),
+        ),
+      );
+
+      // ローディングが表示されるはず
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      // 読み込みが完了したらローディングは表示されないはず
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
+    testWidgets('loading=falseでローディングが表示されるはず', (tester) async {
+      await tester.pumpWidget(
+        mockGitHubSearchApp(
+          home: CachedCircleAvatar(
+            cacheManager: mockCacheManager,
+            url: dummyUrl,
+            loading: false,
+          ),
+        ),
+      );
+
+      // ローディングが表示されないはず
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+
+      await tester.pumpAndSettle();
+
+      // 読み込みが完了してもローディングは表示されないはず
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
+  });
+}
