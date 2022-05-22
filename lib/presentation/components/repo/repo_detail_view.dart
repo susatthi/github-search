bd000db06e082db380e7194d9fdebb8383b076bf
susa.keyber <susa@keyber.jp>
2022-05-22 10:50:28 +0900

    ♻️ refactor:  ネストが深いのでWidgetクラスに切り出し
    Issue #132

--- lib/presentation/components/repo/repo_detail_view.dart
+++ lib/presentation/components/repo/repo_detail_view.dart
@@ -16,6 +16,12 @@ import '../common/preview_avatar_view.dart';
 import 'repo_detail_view_notifier.dart';
 import 'repo_language_label.dart';
 
+/// 1行の縦方向のパディング
+const _verticalPadding = 8.0;
+
+/// 1行の横方向のパディング
+const _horizontalPadding = 16.0;
+
 /// Sliver版リポジトリ詳細View
 class SliverRepoDetailView extends ConsumerWidget {
   const SliverRepoDetailView({super.key});
@@ -50,142 +56,204 @@ class SliverRepoDetailViewInternal extends StatelessWidget {
 
   final RepoData data;
 
-  /// 1行の縦方向のパディング
-  static const _verticalPadding = 8.0;
-
-  /// 1行の横方向のパディング
-  static const _horizontalPadding = 16.0;
-
   @override
   Widget build(BuildContext context) {
     return SliverFillRemaining(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Center(
-            child: MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: InkWell(
-                onTap: () async {
-                  logger.i('Tapped avatar');
-                  await showDialog<void>(
-                    context: context,
-                    builder: (context) => PreviewAvatarView(
-                      url: data.owner.avatarUrl,
-                    ),
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(_verticalPadding),
-                  child: CachedCircleAvatar(
-                    size: 100,
-                    url: data.owner.avatarUrl,
-                  ),
-                ),
+          _AvatarRow(data: data),
+          _FullnameRow(data: data),
+          _DescriptionRow(data: data),
+          _LanguageRow(data: data),
+          _CountRow(data: data),
+        ],
+      ),
+    );
+  }
+}
+
+/// アバター画像
+class _AvatarRow extends StatelessWidget {
+  const _AvatarRow({
+    required this.data,
+  });
+
+  final RepoData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: InkWell(
+          onTap: () async {
+            logger.i('Tapped avatar');
+            await showDialog<void>(
+              context: context,
+              builder: (context) => PreviewAvatarView(
+                url: data.owner.avatarUrl,
               ),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(_verticalPadding),
+            child: CachedCircleAvatar(
+              size: 100,
+              url: data.owner.avatarUrl,
             ),
           ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: _horizontalPadding / 2),
-            child: DefaultTextStyle.merge(
-              style: Theme.of(context).textTheme.titleLarge,
-              child: Row(
-                children: [
-                  HyperlinkText(
-                    onTap: data.owner.ownerUrl != null
-                        ? () => launchUrlInApp(data.owner.ownerUrl!)
-                        : null,
-                    text: data.owner.name,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: _horizontalPadding / 2,
-                      vertical: _verticalPadding,
-                    ),
-                  ),
-                  const Text('/'),
-                  HyperlinkText(
-                    onTap: data.repoUrl != null
-                        ? () => launchUrlInApp(data.repoUrl!)
-                        : null,
-                    text: data.name,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: _horizontalPadding / 2,
-                      vertical: _verticalPadding,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Visibility(
-            visible: data.description?.isNotEmpty == true,
-            child: Padding(
+        ),
+      ),
+    );
+  }
+}
+
+/// リポジトリのフルネーム
+class _FullnameRow extends StatelessWidget {
+  const _FullnameRow({
+    required this.data,
+  });
+
+  final RepoData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: _horizontalPadding / 2),
+      child: DefaultTextStyle.merge(
+        style: Theme.of(context).textTheme.titleLarge,
+        child: Row(
+          children: [
+            HyperlinkText(
+              onTap: data.owner.ownerUrl != null
+                  ? () => launchUrlInApp(data.owner.ownerUrl!)
+                  : null,
+              text: data.owner.name,
               padding: const EdgeInsets.symmetric(
-                horizontal: _horizontalPadding,
+                horizontal: _horizontalPadding / 2,
                 vertical: _verticalPadding,
               ),
-              child: Text(
-                data.description ?? '',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
             ),
-          ),
-          Visibility(
-            visible: data.language != null,
-            child: Padding(
+            const Text('/'),
+            HyperlinkText(
+              onTap: data.repoUrl != null
+                  ? () => launchUrlInApp(data.repoUrl!)
+                  : null,
+              text: data.name,
               padding: const EdgeInsets.symmetric(
-                horizontal: _horizontalPadding,
+                horizontal: _horizontalPadding / 2,
                 vertical: _verticalPadding,
               ),
-              child: RepoLanguageLabel(
-                color: data.languageColor,
-                language: data.language,
-              ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// 説明
+class _DescriptionRow extends StatelessWidget {
+  const _DescriptionRow({
+    required this.data,
+  });
+
+  final RepoData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: data.description?.isNotEmpty == true,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: _horizontalPadding,
+          vertical: _verticalPadding,
+        ),
+        child: Text(
+          data.description ?? '',
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+      ),
+    );
+  }
+}
+
+/// プロジェクト言語
+class _LanguageRow extends StatelessWidget {
+  const _LanguageRow({
+    required this.data,
+  });
+
+  final RepoData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: data.language != null,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: _horizontalPadding,
+          vertical: _verticalPadding,
+        ),
+        child: RepoLanguageLabel(
+          color: data.languageColor,
+          language: data.language,
+        ),
+      ),
+    );
+  }
+}
+
+/// 各数値
+class _CountRow extends StatelessWidget {
+  const _CountRow({
+    required this.data,
+  });
+
+  final RepoData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: _horizontalPadding / 2),
+      child: Row(
+        children: [
+          _IconLabel(
+            padding: const EdgeInsets.symmetric(
+              horizontal: _horizontalPadding / 2,
+              vertical: _verticalPadding,
+            ),
+            url: data.stargazersUrl,
+            icon: Icons.star_outline,
+            text: data.stargazersCountShort,
           ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: _horizontalPadding / 2),
-            child: Row(
-              children: [
-                _IconLabel(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: _horizontalPadding / 2,
-                    vertical: _verticalPadding,
-                  ),
-                  url: data.stargazersUrl,
-                  icon: Icons.star_outline,
-                  text: data.stargazersCountShort,
-                ),
-                _IconLabel(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: _horizontalPadding / 2,
-                    vertical: _verticalPadding,
-                  ),
-                  url: data.watchersUrl,
-                  icon: Icons.visibility_outlined,
-                  text: data.watchersCountShort,
-                ),
-                _IconLabel(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: _horizontalPadding / 2,
-                    vertical: _verticalPadding,
-                  ),
-                  url: data.forksUrl,
-                  icon: Icons.fork_right_outlined,
-                  text: data.forksCountShort,
-                ),
-                _IconLabel(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: _horizontalPadding / 2,
-                    vertical: _verticalPadding,
-                  ),
-                  url: data.issuesUrl,
-                  icon: Icons.bug_report_outlined,
-                  text: data.openIssuesCountShort,
-                ),
-              ],
+          _IconLabel(
+            padding: const EdgeInsets.symmetric(
+              horizontal: _horizontalPadding / 2,
+              vertical: _verticalPadding,
             ),
+            url: data.watchersUrl,
+            icon: Icons.visibility_outlined,
+            text: data.watchersCountShort,
+          ),
+          _IconLabel(
+            padding: const EdgeInsets.symmetric(
+              horizontal: _horizontalPadding / 2,
+              vertical: _verticalPadding,
+            ),
+            url: data.forksUrl,
+            icon: Icons.fork_right_outlined,
+            text: data.forksCountShort,
+          ),
+          _IconLabel(
+            padding: const EdgeInsets.symmetric(
+              horizontal: _horizontalPadding / 2,
+              vertical: _verticalPadding,
+            ),
+            url: data.issuesUrl,
+            icon: Icons.bug_report_outlined,
+            text: data.openIssuesCountShort,
           ),
         ],
       ),
