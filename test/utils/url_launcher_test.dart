2eb312b43b779d8e2f4bb382541cf31c34462f34
susa.keyber <susa@keyber.jp>
2022-05-24 09:36:39 +0900

    💚 test:  カバレッジ上げるためにテストを修正
    Issue #132

--- test/utils/url_launcher_test.dart
+++ test/utils/url_launcher_test.dart
@@ -2,13 +2,35 @@
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github_search/utils/url_launcher.dart';
 import 'package:mocktail/mocktail.dart';
 // ignore: depend_on_referenced_packages
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+// ignore: depend_on_referenced_packages
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
 import '../test_utils/mocks.dart';
+
+/// PlatformException を throw する UrlLauncherPlatform
+class ErrorkUrlLauncherPlatform extends Mock
+    with MockPlatformInterfaceMixin
+    implements UrlLauncherPlatform {
+  @override
+  Future<bool> launch(
+    String url, {
+    required bool useSafariVC,
+    required bool useWebView,
+    required bool enableJavaScript,
+    required bool enableDomStorage,
+    required bool universalLinksOnly,
+    required Map<String, String> headers,
+    String? webOnlyWindowName,
+  }) {
+    throw PlatformException(code: 'code');
+  }
+}
 
 void main() {
   late MockUrlLauncherPlatform mockUrlLauncherPlatform;
@@ -110,6 +132,26 @@ void main() {
           await launchUrlInApp(urlString);
         },
         throwsArgumentError,
+      );
+    });
+    test('PlatformExceptionがthrowされたら開かないはず', () async {
+      final errorUrlLauncherPlatform = ErrorkUrlLauncherPlatform();
+      UrlLauncherPlatform.instance = errorUrlLauncherPlatform;
+
+      const urlString = 'https://github.com';
+
+      await launchUrlInApp(urlString);
+
+      verifyNever(
+        () => mockUrlLauncherPlatform.launch(
+          urlString,
+          useSafariVC: true,
+          useWebView: true,
+          enableJavaScript: true,
+          enableDomStorage: true,
+          universalLinksOnly: false,
+          headers: {},
+        ),
       );
     });
   });
